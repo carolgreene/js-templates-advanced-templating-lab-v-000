55fd7881807eb747b4974fc326deb2e199196814
Carol Greene <carolgreene3@gmail.com>
2018-11-18 17:19:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 function init() {
 let formTemplate = document.getElementById("recipe-form-template").innerHTML;
 let formTemplateFn = Handlebars.compile(formTemplate);
-document.getElementById('main').innerHTML = formTemplateFn({ingredients: ['','','','','']})
+document.getElementById('main').innerHTML = formTemplateFn({ingredients: ['','','','','']});
 }
 
 
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function handleSubmit() {
+  var recipe = {}
+  var nameNode = document.getElementById('name');
+  var descriptionNode = document.getElementById('description');
+  var ingredientNodes = document.getElementsByName('ingredients');
+  recipe.name = nameNode.value;
+  recipe.description = descriptionNode.value;
+  recipe.ingredients = [];
+  for(var i = 0 ; i < ingredientNodes.length ; i++) {
+    recipe.ingredients.push(ingredientNodes[i].value);
+  }
+  var recipeTemplate = document.getElementById("recipe-template").innerHTML;
+  var recipeTemplateFn = Handlebars.compile(recipeTemplate);
+  document.getElementById('main').innerHTML = recipeTemplateFn(recipe);
+}
