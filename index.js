e682917e2e189818c99b4a96d815cfef1f6451a5
Carol Greene <carolgreene3@gmail.com>
2018-11-18 17:56:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let formTemplate = document.getElementById("recipe-form-template").innerHTML;
 let formTemplateFn = Handlebars.compile(formTemplate);
 document.getElementById('main').innerHTML = formTemplateFn({ingredients: ['','','','','']});
 }
+Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
 
 
 document.addEventListener("DOMContentLoaded", function(event) {
