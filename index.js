afdc1adafc54bfda9760bca390a11006805562ec
Carol Greene <carolgreene3@gmail.com>
2018-11-18 17:21:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,34 +9,4 @@ document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
 
-function handleSubmit() {
-  var recipe = {}
-  var nameNode = document.getElementById('name');
-  var descriptionNode = document.getElementById('description');
-  var ingredientNodes = document.getElementsByName('ingredients');
-  recipe.name = nameNode.value;
-  recipe.description = descriptionNode.value;
-  recipe.ingredients = [];
-  for(var i = 0 ; i < ingredientNodes.length ; i++) {
-    recipe.ingredients.push(ingredientNodes[i].value);
-  }
-  var recipeTemplate = document.getElementById("recipe-template").innerHTML;
-  var recipeTemplateFn = Handlebars.compile(recipeTemplate);
-  document.getElementById('main').innerHTML = recipeTemplateFn(recipe);
-}
 
-function displayEditForm() {
-  var recipe = {}
-  var nameNode = document.getElementById('name');
-  var descriptionNode = document.getElementById('description');
-  var ingredientNodes = document.getElementsByName('ingredients');
-  recipe.name = nameNode.innerHTML;
-  recipe.description = descriptionNode.innerHTML;
-  recipe.ingredients = [];
-  for(var i = 0 ; i < ingredientNodes.length ; i++) {
-    recipe.ingredients.push(ingredientNodes[i].innerHTML);
-  }
-  var recipeFormTemplate = document.getElementById("recipe-form-template").innerHTML;
-  var recipeFormTemplateFn = Handlebars.compile(recipeFormTemplate);
-  document.getElementById('main').innerHTML = recipeFormTemplateFn(recipe);
-}
